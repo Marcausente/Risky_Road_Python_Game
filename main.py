baf2b716db72a2f3b8baabbc5bcb7f10252a5b2c
Marc Fernández <marcausente@gmail.com>
2024-10-30 16:03:45 +0100

    Bucle para matener ventana

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import pygame, sys
 from pygame.locals import  *
 
 pygame.init()
+
+pantalla = pygame.display.set_mode((800, 700)) #Basicamente crea una ventana de 500x400
+pygame.display.set_caption("Risky Road")
+
+while True: #Este bucle se enarga de mantenter la pantalla abierta, si no se cierra solo
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
