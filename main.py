74ccd853b508b2bc8ffb1d186c3a80e2bac5c26c
Marc Fernández <marcausente@gmail.com>
2024-10-31 20:37:28 +0100

    Fix direcciones de movimiento

--- main.py
+++ main.py
@@ -16,28 +16,30 @@ pantalla.blit(fondo, (0, 0))
 quietoarriba = pygame.image.load("img/MainCharacter/MainUpStanding.png")
 caminaarriba1 = pygame.image.load("img/MainCharacter/MainUpWalking1.png")
 caminaarriba2 = pygame.image.load("img/MainCharacter/MainUpWalking2.png")
-quietoabajo = pygame.image.load("img\MainCharacter\MainDownStanding.png")
-caminaabajo1 = pygame.image.load("img\MainCharacter\MainDownWalking1.png")
-caminaabajo2 = pygame.image.load("img\MainCharacter\MainDownWalking2.png")
-quietoderecha = pygame.image.load("img\MainCharacter\MainDerechaStanding.png")
-caminaderecha1 = pygame.image.load("img\MainCharacter\MainDerechaWalking1.png")
-caminaderecha2 = pygame.image.load("img\MainCharacter\MainDerechaWalking2.png")
-quietoizquierda = pygame.image.load("img\MainCharacter\MainIzquierdaStanding.png")
-caminarizquierda1 = pygame.image.load("img\MainCharacter\MainIzquierdaWalking1.png")
-caminarizquierda2 = pygame.image.load("img\MainCharacter\MainIzquierdaWalking2.png")
+quietoabajo = pygame.image.load("img/MainCharacter/MainDownStanding.png")
+caminaabajo1 = pygame.image.load("img/MainCharacter/MainDownWalking1.png")
+caminaabajo2 = pygame.image.load("img/MainCharacter/MainDownWalking2.png")
+quietoderecha = pygame.image.load("img/MainCharacter/MainDerechaStanding.png")
+caminaderecha1 = pygame.image.load("img/MainCharacter/MainDerechaWalking1.png")
+caminaderecha2 = pygame.image.load("img/MainCharacter/MainDerechaWalking2.png")
+quietoizquierda = pygame.image.load("img/MainCharacter/MainIzquierdaStanding.png")
+caminarizquierda1 = pygame.image.load("img/MainCharacter/MainIzquierdaWalking1.png")
+caminarizquierda2 = pygame.image.load("img/MainCharacter/MainIzquierdaWalking2.png")
+
 # Redimensionamos las imágenes del personaje
 quietoarriba = pygame.transform.scale(quietoarriba, (60, 60))
 caminaarriba1 = pygame.transform.scale(caminaarriba1, (60, 60))
 caminaarriba2 = pygame.transform.scale(caminaarriba2, (60, 60))
-quietoabajo = pygame.transform.scale(quietoabajo, (60,60))
-caminaabajo1 = pygame.transform.scale(caminaabajo1, (60,60))
-caminaabajo2 = pygame.transform.scale(caminaabajo2, (60,60))
-quietoderecha = pygame.transform.scale(quietoderecha, (60,60))
-caminaderecha1 = pygame.transform.scale(caminaderecha1, (60,60))
-caminaderecha2 = pygame.transform.scale(caminaderecha2, (60,60))
-quietoizquierda = pygame.transform.scale(quietoizquierda, (60,60))
-caminarizquierda1 = pygame.transform.scale(caminarizquierda1, (60,60))
-caminarizquierda2 = pygame.transform.scale(caminarizquierda2, (60,60))
+quietoabajo = pygame.transform.scale(quietoabajo, (60, 60))
+caminaabajo1 = pygame.transform.scale(caminaabajo1, (60, 60))
+caminaabajo2 = pygame.transform.scale(caminaabajo2, (60, 60))
+quietoderecha = pygame.transform.scale(quietoderecha, (60, 60))
+caminaderecha1 = pygame.transform.scale(caminaderecha1, (60, 60))
+caminaderecha2 = pygame.transform.scale(caminaderecha2, (60, 60))
+quietoizquierda = pygame.transform.scale(quietoizquierda, (60, 60))
+caminarizquierda1 = pygame.transform.scale(caminarizquierda1, (60, 60))
+caminarizquierda2 = pygame.transform.scale(caminarizquierda2, (60, 60))
+
 # Lista de imágenes para la animación
 imagenes_caminar_arriba = [quietoarriba, caminaarriba1, quietoarriba, caminaarriba2]
 imagenes_caminar_abajo = [quietoabajo, caminaabajo1, quietoabajo, caminaabajo2]
@@ -54,6 +56,9 @@ velocidad = 0.15  # Velocidad de movimiento
 ancho_pantalla, alto_pantalla = pantalla.get_size()
 ancho_personaje, alto_personaje = quietoarriba.get_size()
 
+# Variable para registrar la última dirección
+ultima_direccion = "arriba"  # Se inicializa con la dirección hacia arriba
+
 while True:  # Bucle para mantener la pantalla abierta
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -63,39 +68,49 @@ while True:  # Bucle para mantener la pantalla abierta
     # Teclas presionadas
     teclas = pygame.key.get_pressed()
 
-    # Movimiento del personaje
+    # Movimiento del personaje y animación
     if teclas[K_w]:
         pos_y -= velocidad
-        # Actualizar la animación cada 200 ms
+        ultima_direccion = "arriba"
         if pygame.time.get_ticks() - tiempo_animacion > 100:
             indice_anim = (indice_anim + 1) % len(imagenes_caminar_arriba)
             tiempo_animacion = pygame.time.get_ticks()
-
         imagen_actual = imagenes_caminar_arriba[indice_anim]
-    else:
-        imagen_actual = quietoarriba  # Si no se presiona "W", mostrar la imagen quieto
-    if teclas[K_s]:
+
+    elif teclas[K_s]:
         pos_y += velocidad
+        ultima_direccion = "abajo"
         if pygame.time.get_ticks() - tiempo_animacion > 100:
             indice_anim = (indice_anim + 1) % len(imagenes_caminar_abajo)
             tiempo_animacion = pygame.time.get_ticks()
-
         imagen_actual = imagenes_caminar_abajo[indice_anim]
 
-    if teclas[K_a]:
+    elif teclas[K_a]:
         pos_x -= velocidad
+        ultima_direccion = "izquierda"
         if pygame.time.get_ticks() - tiempo_animacion > 100:
             indice_anim = (indice_anim + 1) % len(imagenes_caminar_izquierda)
             tiempo_animacion = pygame.time.get_ticks()
-
         imagen_actual = imagenes_caminar_izquierda[indice_anim]
-    if teclas[K_d]:
+
+    elif teclas[K_d]:
         pos_x += velocidad
+        ultima_direccion = "derecha"
         if pygame.time.get_ticks() - tiempo_animacion > 100:
             indice_anim = (indice_anim + 1) % len(imagenes_caminar_derecha)
             tiempo_animacion = pygame.time.get_ticks()
-
         imagen_actual = imagenes_caminar_derecha[indice_anim]
+
+    else:
+        # Selecciona la imagen quieta según la última dirección
+        if ultima_direccion == "arriba":
+            imagen_actual = quietoarriba
+        elif ultima_direccion == "abajo":
+            imagen_actual = quietoabajo
+        elif ultima_direccion == "derecha":
+            imagen_actual = quietoderecha
+        elif ultima_direccion == "izquierda":
+            imagen_actual = quietoizquierda
 
     # Limitar la posición dentro de los bordes de la pantalla
     pos_x = max(0, min(pos_x, ancho_pantalla - ancho_personaje))
