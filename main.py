1f7f8ac2990b28e1657799ea86db3b6b3695cd59
Marc Fernández <marcausente@gmail.com>
2024-10-31 03:44:53 +0100

    Mostrar personaje y movimiento

--- main.py
+++ main.py
@@ -19,12 +19,33 @@ GREY = (76, 76, 78, 1)
 
 fondo = pygame.image.load("img/GameBackground.jpg")
 pantalla.blit(fondo, (0, 0))
-
 quieto = pygame.image.load("img/MainCharacter/MainUpStanding.png")
+quieto = pygame.transform.scale(quieto, (60, 60)) #Redimensiona al personaje porque si no no aparece
+
+pos_x, pos_y = 375, 375 #Esto nos coloca al personaje en el centro de la pantalla
+velocidad = 0.15 #Velocidad, es baja porque es pequeño el mapa
 
 while True: #Este bucle se enarga de mantenter la pantalla abierta, si no se cierra solo
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    pygame.display.update() #Actualiza el color de la pantalla al gris que hemos puesto antes
+
+    # Basicamente mira que tecla hay presionada
+    teclas = pygame.key.get_pressed()
+
+    # Movimiento del personaje
+    if teclas[K_w]:
+        pos_y -= velocidad
+    if teclas[K_s]:
+        pos_y += velocidad
+    if teclas[K_a]:
+        pos_x -= velocidad
+    if teclas[K_d]:
+        pos_x += velocidad
+
+    # Dibujar el fondo y el personaje en la nueva posición
+    pantalla.blit(fondo, (0, 0))  # Dibuja el fondo
+    pantalla.blit(quieto, (pos_x, pos_y))  # Dibuja el personaje
+
+    pygame.display.update()  # Actualiza la pantallawa
